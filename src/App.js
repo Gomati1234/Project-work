ef90c6b0dbc1575a526b5ae26e840c2254483d5b
Aravindh036 <32773583+Aravindh036@users.noreply.github.com>
2018-02-21 23:22:06 +0530

    button added with image

--- src/App.js
+++ src/App.js
@@ -4,9 +4,8 @@ import './App.css';
 import GoogleLogin from 'react-google-login';
 import FacebookLogin from 'react-facebook-login';
 import GitHubLogin from 'react-github-login';
-
-
-
+import google from './assets/google.png';
+import github from './assets/github.png';
 
 const MyFacebookButton = ({ onClick }) => (
   <button onClick={onClick}>
@@ -23,36 +22,42 @@ class App extends Component {
     }
     const onSuccess = response => console.log("Success   "+JSON.stringify(response));
     const onFailure = response => console.log("Failed here "+response);
-
+    const styleList={
+      color:'white'
+    }
 return (
         <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
+          <div className="googleLogin">
           <GoogleLogin
+          style={styleList}
             clientId="324956446559-mb14m1p3pmvv2jnobvj2q0glsheo9g62.apps.googleusercontent.com"
-            buttonText="LOGIN WITH GOOGLE"
             onSuccess={responseGoogle}
             onFailure={responseGoogle}
-          />
+          ><img src={google} alt="hello"/></GoogleLogin>
+          </div>
           
+          <div className="facebookLogin">
           <FacebookLogin
+          style={styleList}
+          textButton=""
+          cssClass="facebook"
             appId="815415458651207"
             // autoLoad={true}
             fields="name,email,picture"
             callback={responseFacebook} 
             component = {MyFacebookButton}
           />
+          </div>
+
+          <div className="githubLogin">
           <GitHubLogin 
+          style={styleList}          
               redirectUri="" 
               clientId="815b62d2de3f985b85da"
               onSuccess={onSuccess}
               onFailure={onFailure}
-          />
+          ><img src={github} alt="hello"/></GitHubLogin>
+          </div>
          
         </div>
 );
